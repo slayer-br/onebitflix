b4bb7ccfdae3837e7c6f2aef4582bf4739f24b4f
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-14 18:19:44 -0300

    feat: adiciona model, migration e resource de categorias no AdminJS

---
+++ src/adminjs/resources/index.ts
@@ -0,0 +1,5 @@
+import { ResourceWithOptions } from "adminjs";
+import { Category } from "../../models";
+import { categoryResourceOptions } from "./category";
+
+export const adminJsResources: ResourceWithOptions[] = [{ resource: Category, options: categoryResourceOptions }];
