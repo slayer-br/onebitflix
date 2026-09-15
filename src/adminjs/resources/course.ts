f97b701eb04f0554751810bc4173489e25678754
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-15 18:00:31 -0300

    feat(adminjs): ajusta recurso de cursos e ignora pasta de uploads

--- src/adminjs/resources/course.ts
+++ src/adminjs/resources/course.ts
@@ -1,4 +1,6 @@
-import { ResourceOptions } from "adminjs";
+import path from "path";
+import uploadFileFeature from "@adminjs/upload";
+import { ResourceOptions, FeatureType } from "adminjs";
 
 export const courseResourceOptions: ResourceOptions = {
   navigation: "Catálogo",
@@ -8,3 +10,17 @@ export const courseResourceOptions: ResourceOptions = {
   showProperties: ["id", "name", "synopsis", "featured", "thumbnailUrl", "categoryId", "createdAt", "updatedAt"],
 };
 
+export const courseResourceFeatures: FeatureType[] = [
+  uploadFileFeature({
+    provider: {
+      local: {
+        bucket: path.join(__dirname, "public"),
+      },
+    },
+    properties: {
+      key: "thumbnailUrl",
+      file: "uploadThumbnail",
+    },
+    uploadPath: (record, filename) => `thumbnails/course-${record.get("id")}/${filename}`,
+  }),
+];
