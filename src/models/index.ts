e4b15d19199bf6f69d817cda15299a5d55916e5f
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-16 18:50:55 -0300

    feat: implementa migration, model, service, controller e rotas de favoritos

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,16 +1,22 @@
 import { Category } from "./Category";
 import { Course } from "./Course";
-import { Episode } from "./Episode"
+import { Episode } from "./Episode";
+import { Favorite } from "./Favorite";
 import { User } from "./User";
 
-Category.hasMany(Course, {as: 'courses'})
-Course.belongsTo(Category)
+Category.hasMany(Course, { as: "courses" });
 
-Course.hasMany(Episode , { as: 'episodes' })
-Episode.belongsTo(Course)
-export {
-  Category,
-  Course,
-  Episode, 
-  User
-};
+Course.belongsTo(Category);
+Course.hasMany(Episode);
+Course.belongsToMany(User, { through: Favorite });
+Course.hasMany(Favorite, { as: "favoritesUsers", foreignKey: "course_id" });
+
+Episode.belongsTo(Course);
+
+Favorite.belongsTo(Course);
+Favorite.belongsTo(User);
+
+User.belongsToMany(Course, { through: Favorite });
+User.hasMany(Favorite, { as: "favoritesCourses", foreignKey: "user_id" });
+
+export { Category, Course, Episode, Favorite, User };
--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,10 @@ import express from "express";
 import { categoriesController } from "./controllers/categoriesController";
 import { coursesController } from "./controllers/coursesController";
 import { episodesController } from "./controllers/episodesController";
+import {favoriteController} from "./controllers/favoriteController";
 import { authController } from "./controllers/authController";
 import { ensureAuth, ensureAuthViaQuery } from "./middlewares/auth";
+
 
 const router = express.Router();
 
@@ -20,4 +22,6 @@ router.get("/courses/search", ensureAuth,coursesController.search);
 router.get("/courses/:id", ensureAuth,coursesController.show);
 
 router.get("/episodes/stream",ensureAuthViaQuery, episodesController.stream);
+
+router.post('/favorites', ensureAuth, favoriteController.save)
 export { router };
