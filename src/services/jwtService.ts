4bb15491a696f7ff66710c28bc9de3fc5982a981
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-16 18:12:54 -0300

    feat: implementa middleware de autenticacao e protecao de rotas via JWT

--- src/services/jwtService.ts
+++ src/services/jwtService.ts
@@ -8,4 +8,8 @@ export const jwtService = {
   signToken: (payload: string | object | Buffer, expiration: SignOptions["expiresIn"]) => {
     return jwt.sign(payload, secret, { expiresIn: expiration });
   },
+
+  verifyToken: (token: string,  callbackfn: jwt.VerifyCallback) => {
+    jwt.verify(token, secret, callbackfn);
+  }
 };
