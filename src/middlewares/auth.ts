d5853061bb45f93c77b85bf13ec27042c5409f12
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-16 18:27:59 -0300

    feat: adiciona autenticacao via middleware nas rotas de streaming de video

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -15,13 +15,34 @@ export function ensureAuth(req: AuthenticatedRequest, res: Response, next: NextF
   }
   const token = authorizationHeader.replace(/Bearer /, "");
 
-  jwtService.verifyToken(token, (err, decoded) => {
+  jwtService.verifyToken(token, async (err, decoded) => {
     if (err || typeof decoded === "undefined") {
       return res.status(401).json({ message: "Não autorizado: Token inválido." });
     }
-    userService.findByEmail((decoded as JwtPayload).email).then((user) => {
-      req.user = user;
-      next();
-    });
+    const user = await userService.findByEmail((decoded as JwtPayload).email);
+    req.user = user;
+    next();
+  });
+}
+
+export function ensureAuthViaQuery(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const { token } = req.query;
+
+  if (!token) {
+    return res.status(401).json({ message: "Não autorizado: nenhum Token fornecido." });
+  }
+
+  if (typeof token !== "string") {
+    return res.status(400).json({ message: "O parâmetro token deve ser do tipo string" });
+  }
+
+  jwtService.verifyToken(token, async (err, decoded) => {
+    if (err || typeof decoded === "undefined") {
+      return res.status(401).json({ message: "Não autorizado: Token inválido." });
+    }
+
+    const user = await userService.findByEmail((decoded as JwtPayload).email);
+    req.user = user;
+    next();
   });
 }
