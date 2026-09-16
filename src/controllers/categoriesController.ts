73e0c17066bcc7e9690f7dade58381a2168e0ec4
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-16 14:54:03 -0300

    feat: implementa camada de services, helpers e atualiza categoriesController

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -1,17 +1,23 @@
-import { Request, Response } from "express";
-import { Category } from "../models";
-export const categoriesController = {
+// src/controllers/categories-controller.ts
+
+import { Request, Response } from 'express'
+import { getPaginationParams } from '../helpers/getPaginationParams'
+import { categoryService } from '../services/categoryService'
+
+const categoriesController = {
   index: async (req: Request, res: Response) => {
+    const [page, perPage] = getPaginationParams(req.query)
+
     try {
-      const categories = await Category.findAll({
-        attributes: ["id", "name", "position"],
-        order: [["position", "ASC"]],
-      });
-      return res.json(categories);
-    } catch (error) {
-      if (error instanceof Error) {
-        return res.status(400).json({ message: error.message });
+      const paginatedCategories = await categoryService.findAllPaginated(page, perPage)
+
+      return res.json(paginatedCategories)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
       }
     }
-  },
-};
+  }
+}
+
+export { categoriesController }
