851d28854ccf802ba448f053dbed0f31e6f38f51
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-16 15:58:22 -0300

    feat: adiciona paginacao na busca de cursos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { courseService } from "../services/courseService";
+import { getPaginationParams } from "../helpers/getPaginationParams";
 
 export const coursesController = {
   show: async (req: Request, res: Response) => {
@@ -36,17 +37,18 @@ export const coursesController = {
     }
   },
 
-  search: async (req: Request, res: Response) => {
-    const { name } = req.query;
+search: async (req: Request, res: Response) => {
+    const { name } = req.query
+    const [page, perPage] = getPaginationParams(req.query)
 
     try {
-      if (typeof name !== "string") throw new Error("name param must be of type string");
-      const courses = await courseService.findByName(name);
-      return res.json(courses);
+			if (typeof name !== 'string') throw new Error('name param must be of type string');
+      const courses = await courseService.findByName(name, page, perPage)
+      return res.json(courses)
     } catch (err) {
       if (err instanceof Error) {
-        return res.status(400).json({ message: err.message });
+        return res.status(400).json({ message: err.message })
       }
     }
-  },
+  }
 };
