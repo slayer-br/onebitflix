d20c9b130ea4594c28e819e4835b6ec2e3da156d
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-16 17:41:53 -0300

    feat: implementa autenticacao baseada em JWT (jwtService, rotas e dependencias)

---
+++ src/services/jwtService.ts
@@ -0,0 +1,11 @@
+// src/services/jwtService.ts
+
+import jwt, { SignOptions } from "jsonwebtoken";
+
+const secret = "chave-jwt";
+
+export const jwtService = {
+  signToken: (payload: string | object | Buffer, expiration: SignOptions["expiresIn"]) => {
+    return jwt.sign(payload, secret, { expiresIn: expiration });
+  },
+};
