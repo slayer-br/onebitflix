5c44e771849019b6d4d721af022e9db3b01f78e9
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-16 15:41:08 -0300

    feat: implementa busca e rota de lancamentos de cursos

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,36 +1,39 @@
-import { Course } from "../models"
+import { Course } from "../models";
 
 export const courseService = {
   findByIdWithEpisodes: async (id: string) => {
     const courseWithEpisodes = await Course.findByPk(id, {
-      attributes: ['id', 'name', 'synopsis', ['thumbnail_url', 'thumbnailUrl']],
+      attributes: ["id", "name", "synopsis", ["thumbnail_url", "thumbnailUrl"]],
       include: {
-        association: 'episodes',
-        attributes: [
-          'id',
-          'name',
-          'synopsis',
-          'order',
-          ['video_url', 'videoUrl'],
-          ['seconds_long', 'secondsLong']
-        ],
-        order: [['order', 'ASC']],
-        separate: true
-      }
-    })
+        association: "episodes",
+        attributes: ["id", "name", "synopsis", "order", ["video_url", "videoUrl"], ["seconds_long", "secondsLong"]],
+        order: [["order", "ASC"]],
+        separate: true,
+      },
+    });
 
-    return courseWithEpisodes
+    return courseWithEpisodes;
   },
+
   getRandomFeaturedCourses: async () => {
     const featuredCourses = await Course.findAll({
-      attributes: ['id', 'name', 'synopsis', ['thumbnail_url', 'thumbnailUrl']],
+      attributes: ["id", "name", "synopsis", ["thumbnail_url", "thumbnailUrl"]],
       where: {
-        featured: true
-      }
-    })
+        featured: true,
+      },
+    });
 
-    const randomFeaturedCourses = featuredCourses.sort(() => 0.5 - Math.random())
+    const randomFeaturedCourses = featuredCourses.sort(() => 0.5 - Math.random());
 
-    return randomFeaturedCourses.slice(0, 3)
+    return randomFeaturedCourses.slice(0, 3);
   },
-}
+
+  getTopTenNewest: async () => {
+    const courses = await Course.findAll({
+      limit: 10,
+      order: [["created_at", "DESC"]],
+    });
+
+    return courses;
+  },
+};
