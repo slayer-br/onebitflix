3424e5b7c9951c60ba461ff06a46e6a29a8c7101
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-16 15:27:50 -0300

    feat: atualiza controller, service e rotas do modulo de cursos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,18 +1,27 @@
-
-
-import { Request, Response } from 'express'
-import { courseService } from '../services/courseService';
+import { Request, Response } from "express";
+import { courseService } from "../services/courseService";
 
 export const coursesController = {
   show: async (req: Request, res: Response) => {
-    const { id } = req.params
+    const { id } = req.params;
     try {
-      const course = await courseService.findByIdWithEpisodes(id)
-      return res.json(course)
+      const course = await courseService.findByIdWithEpisodes(id);
+      return res.json(course);
     } catch (err) {
       if (err instanceof Error) {
-        return res.status(400).json({ message: err.message })
+        return res.status(400).json({ message: err.message });
       }
     }
-  }
-}
+  },
+
+  featured: async (req: Request, res: Response) => {
+    try {
+      const featuredCourses = await courseService.getRandomFeaturedCourses();
+      return res.json(featuredCourses);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+};
