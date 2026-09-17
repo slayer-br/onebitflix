4c4ced63a1a3871a451ea673d4ea954b52d1b87b
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-17 14:41:07 -0300

    feat: adiciona rota para obter informacoes da conta do usuario

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,17 +3,27 @@ import { AuthenticatedRequest } from "../middlewares/auth";
 import { userService } from "../services/userService";
 
 export const usersController = {
-  // GET /users/current/watching
-  watching: async (req: AuthenticatedRequest, res: Response) => {
-    const { id } = req.user!
-
+  show: async (req: AuthenticatedRequest, res: Response) => {
     try {
-      const watching = await userService.getKeepWatchingList(id)
-      return res.json(watching)
+      const currentUser = req.user;
+      return res.json(currentUser);
     } catch (err) {
       if (err instanceof Error) {
-        return res.status(400).json({ message: err.message })
+        return res.status(400).json({ message: err.message });
       }
     }
-  }
-}
+  },
+
+  watching: async (req: AuthenticatedRequest, res: Response) => {
+    const { id } = req.user!;
+
+    try {
+      const watching = await userService.getKeepWatchingList(id);
+      return res.json(watching);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+};
