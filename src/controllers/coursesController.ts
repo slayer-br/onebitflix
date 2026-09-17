8456f20441934127b0592c7d88e58e193213f900
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-17 11:31:48 -0300

    feat: integra likes e favoritos nas rotas de cursos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,13 +1,24 @@
 import { Request, Response } from "express";
 import { courseService } from "../services/courseService";
 import { getPaginationParams } from "../helpers/getPaginationParams";
+import { AuthenticatedRequest } from "../middlewares/auth";
+import { likeService } from "../services/likeService";
+import { favoriteService } from "../services/favoriteService";
 
 export const coursesController = {
-  show: async (req: Request, res: Response) => {
-    const { id } = req.params;
+  show: async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user!.id;
+    const courseId = req.params.id;
+
     try {
-      const course = await courseService.findByIdWithEpisodes(id);
-      return res.json(course);
+      const course = await courseService.findByIdWithEpisodes(courseId);
+
+      if (!course) return res.status(404).json({ message: "Curso não encontrado" });
+
+      const liked = await likeService.isLiked(userId, Number(courseId));
+      const favorited = await favoriteService.isFavorited(userId, Number(courseId));
+
+      return res.json({ ...course.get(), favorited, liked });
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message });
@@ -37,18 +48,18 @@ export const coursesController = {
     }
   },
 
-search: async (req: Request, res: Response) => {
-    const { name } = req.query
-    const [page, perPage] = getPaginationParams(req.query)
+  search: async (req: Request, res: Response) => {
+    const { name } = req.query;
+    const [page, perPage] = getPaginationParams(req.query);
 
     try {
-			if (typeof name !== 'string') throw new Error('name param must be of type string');
-      const courses = await courseService.findByName(name, page, perPage)
-      return res.json(courses)
+      if (typeof name !== "string") throw new Error("name param must be of type string");
+      const courses = await courseService.findByName(name, page, perPage);
+      return res.json(courses);
     } catch (err) {
       if (err instanceof Error) {
-        return res.status(400).json({ message: err.message })
+        return res.status(400).json({ message: err.message });
       }
     }
-  }
+  },
 };
