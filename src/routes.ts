e6fe27ffe889df4e66ce50434887c4594b012640
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-17 09:22:43 -0300

    feat: implementa a exclusao de favoritos

--- src/routes.ts
+++ src/routes.ts
@@ -2,28 +2,27 @@ import express from "express";
 import { categoriesController } from "./controllers/categoriesController";
 import { coursesController } from "./controllers/coursesController";
 import { episodesController } from "./controllers/episodesController";
-import {favoritesController} from "./controllers/favoritesController";
+import { favoritesController } from "./controllers/favoritesController";
 import { authController } from "./controllers/authController";
 import { ensureAuth, ensureAuthViaQuery } from "./middlewares/auth";
 
-
 const router = express.Router();
 
-
-router.post('/auth/register', authController.register)
-router.post('/auth/login', authController.login)
+router.post("/auth/register", authController.register);
+router.post("/auth/login", authController.login);
 
 router.get("/categories", ensureAuth, categoriesController.index);
-router.get("/categories/:id", ensureAuth,categoriesController.show);
+router.get("/categories/:id", ensureAuth, categoriesController.show);
 
-router.get("/courses/featured", ensureAuth,coursesController.featured);
+router.get("/courses/featured", ensureAuth, coursesController.featured);
 router.get("/courses/newest", coursesController.newest);
-router.get("/courses/search", ensureAuth,coursesController.search);
-router.get("/courses/:id", ensureAuth,coursesController.show);
+router.get("/courses/search", ensureAuth, coursesController.search);
+router.get("/courses/:id", ensureAuth, coursesController.show);
 
-router.get("/episodes/stream",ensureAuthViaQuery, episodesController.stream);
+router.get("/episodes/stream", ensureAuthViaQuery, episodesController.stream);
 
-router.get('/favorites', ensureAuth, favoritesController.index)
-router.post('/favorites', ensureAuth, favoritesController.save)
+router.get("/favorites", ensureAuth, favoritesController.index);
+router.post("/favorites", ensureAuth, favoritesController.save);
+router.delete("/favorites", ensureAuth, favoritesController.delete);
 
 export { router };
--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -23,7 +23,7 @@ export const favoriteService = {
       courses: favorites.map(favorite => favorite.Course)
     }
   },
-  
+
   create: async (userId: number, courseId: number) => {
     const favorite = await Favorite.create({
       userId,
@@ -32,4 +32,13 @@ export const favoriteService = {
 
     return favorite
   },
+
+  delete: async (userId: number, courseId: number) => {
+    await Favorite.destroy({
+      where: {
+        userId,
+        courseId
+      }
+    })
+  }
 }
