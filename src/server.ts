ec4383d2d68a15e72866412f382902ede8d70ab6
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-17 15:09:49 -0300

    feat: adiciona e configura o middleware CORS

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
+import cors from "cors";
 import { adminJs, adminJsRouter } from "./adminjs";
 import { sequelize } from "./database";
 import { router } from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.static("public"));
-app.use(express.json())
+app.use(express.json());
 app.use(adminJs.options.rootPath, adminJsRouter);
 app.use(router);
 
