7e3eb65e01547198066736b8ee0bec16cc23adc1
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-17 16:39:16 -0300

    feat: inicializa a estrutura do projeto frontend em Next.js

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,9 @@
+import type { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.scss";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import styles from "../styles/homeNoAuth.module.scss";
+
+const HomeNotAuth = function () {
+  return (
+    <>
+      <Head>
+        <title>OneBitFlix</title>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+      <main>
+      </main>
+    </>
+  );
+};
+
+export default HomeNotAuth;
