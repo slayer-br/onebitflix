2308a9c1262724f7830c037639820983500678f8
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-22 10:52:17 -0300

    feat: adiciona animacoes de rolagem com a biblioteca AOS na home

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,8 +6,10 @@ import PresentationSection from "@/components/homeNoAuth/presentationSection";
 import CardsSection from "@/components/homeNoAuth/cardsSection";
 import SlideSection from "@/components/homeNoAuth/slideSection";
 import courseService, { CourseType } from "@/services/courseService";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Footer from "@/components/common/footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 interface IndexPageProps {
   children?: ReactNode;
@@ -15,6 +17,9 @@ interface IndexPageProps {
 }
 
 const HomeNotAuth = function ({ course }: IndexPageProps) {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <>
       <Head>
@@ -27,13 +32,17 @@ const HomeNotAuth = function ({ course }: IndexPageProps) {
         />
       </Head>
       <main>
-        <div className={styles.sectionBackground}>
+        <div className={styles.sectionBackground} data-aos="fade-zoom-in" data-aos-duration="1600">
           <HeaderNoAuth />
           <PresentationSection />
-          <CardsSection />
-          <SlideSection newestCourses={course} />
-          <Footer />
         </div>
+        <div data-aos="fade-right" data-aos-duration="1200">
+          <CardsSection />
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1200">
+          <SlideSection newestCourses={course} />
+        </div>
+        <Footer />
       </main>
     </>
   );
