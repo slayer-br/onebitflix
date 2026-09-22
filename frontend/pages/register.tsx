31d80dfb3f1bb551d554bcabbd6c450f48c21a8a
Carlos Alberto da Silva <slayer.br@gmail.com>
2026-09-22 12:30:05 -0300

    style: ajusta estilos da pagina e formulario de registro

--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -17,7 +17,7 @@ const Register = function () {
         <Container className="py-5">
           <p className={styles.formTitle}>Bem vindo(a) ao OneBitFlix!</p>
           <Form className={styles.form}>
-            <p className="text-center">
+            <p className={styles.formSubTitle}>
               <strong>Faça seu cadastro</strong>
             </p>
             <FormGroup>
